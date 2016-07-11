6b25bf2f1d1616f8a64177e3c89ec862b5e04735
Felipe Martins <ffmmjj@gmail.com>
2016-07-11 10:30:27 -0300

    Added setup.py to the module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='kairos_face_recognition_lib',
+    version='0.1.0',
+    packages=['kairos_face'],
+    url='https://github.com/ffmmjj/kairos-face-sdk-python',
+    license='MIT',
+    author='Felipe Martins',
+    author_email='',
+    description='Kairos Face Recognition API Python Client Library',
+    install_requires=[
+        'requests'
+    ]
+)
