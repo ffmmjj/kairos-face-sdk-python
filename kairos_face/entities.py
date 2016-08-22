8af44982b11760e31b3906ca91b5ffc0a6dbb15b
Felipe Martins <ffmmjj@gmail.com>
2016-08-22 20:06:38 -0300

    Enhanced string repr of RecognizedFaceCandidate

--- kairos_face/entities.py
+++ kairos_face/entities.py
@@ -2,3 +2,9 @@ class RecognizedFaceCandidate:
     def __init__(self, subject, confidence):
         self.subject = subject
         self.confidence = confidence
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '<Face "{}" with confidence. {}>'.format(self.subject, self.confidence)
